6474ec3df7fe9b78e2c61a80a14b3f37621ba100
dmnhthng <thangdm@metawayholdings.com>
2023-03-10 09:57:54 +0700

    update

--- js/tailwindConfig.js
+++ js/tailwindConfig.js
@@ -2,10 +2,12 @@ tailwind.config = {
   theme: {
     extend: {
         colors: {
-            primary: '#35a736',
+            primary: '#8dc63f',
             lightRed: '#e3af9e14',
             red: '#35a736',
             secondary: '#051f4d',
+            dark: "#111709",
+            grey: '#999',
         }
     }
   }
